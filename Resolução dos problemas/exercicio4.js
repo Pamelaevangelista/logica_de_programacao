d7ee37cd033cf9555ddcacbb4af5a06549f45f49
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-09-25 21:29:22 -0300

    Add files via upload

---
+++ Resolução dos problemas/exercicio4.js
@@ -0,0 +1,8 @@
+//Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+var A = parseInt(20);
+var B = parseInt(20);
+
+var soma = A + B
+
+console.log(soma)
